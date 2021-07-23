eeb1c162aee7b4a91b6cec4b7771c75e0eddc02e
kiyongee2 <kiyongee2@gmail.com>
2021-07-23 13:05:48 +0900

    poll 최종소스

--- poll/urls.py
+++ poll/urls.py
@@ -5,9 +5,14 @@ app_name = 'poll'
 
 urlpatterns = [
     # /poll/
-    path("", views.index),
+    path("", views.index, name='index'),
     # /poll/2
     path("<int:question_id>/", views.detail, name='detail'),
     # /poll/2/vote
     path("<int:question_id>/vote/", views.vote, name='vote'),
+    path("<int:question_id>/results/", views.results, name='results'),
+    # /poll/val_jq
+    path("val_jq/", views.val_jq, name='val_jq'),
+    path("parent_jq/", views.parent_jq, name='parent_jq'),
+    path("submenu/", views.submenu, name="submenu"),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
+from django.urls import reverse
 
 def index(request):
     question_list = Question.objects.order_by('-pub_date')
@@ -8,9 +9,35 @@ def index(request):
     return render(request, 'poll/list.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'poll/detail.html', context)
 
 def vote(request,question_id):
-    return HttpResponse("투표중입니다. %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message': '선택을 해주세요!'
+        })
+    else:
+        selected_choice.votes += 1    #선택 하면 1이 증가함
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'poll/results.html', context)
+
+def val_jq(request):
+    return render(request, 'poll/val_jq.html')
+
+def parent_jq(request):
+    return render(request, 'poll/parent_jq.html')
+
+def submenu(request):
+    return render(request, 'poll/submenu.html')
